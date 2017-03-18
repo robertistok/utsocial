340d88505ecb9497cf0f6e2c04ecc846e3c96f26
Robert Istok <robertistok94@gmail.com>
2017-03-18 18:48:37 +0200

    Server basic setup

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3001);
+
+if (proces.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+}
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+
+app.listen(app.get('port'), () => {
+  console.log(
+    `Up and running!\nFind the server at http://localhost:${app.get('port')}/`
+  );
+});
