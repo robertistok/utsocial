f9b852ddfa8549332dbb9a49682ffa0360f9f5cf
Robert Istok <robertistok94@gmail.com>
2017-03-18 19:09:16 +0200

    Modify

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.set('port', process.env.PORT || 3001);
 
 if (proces.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
 }
 
 module.exports = app;
