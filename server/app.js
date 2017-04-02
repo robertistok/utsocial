c35372c7a587c77a9e7b0148314ea406e8adbcf2
Robert Istok <robertistok94@gmail.com>
2017-04-02 15:50:50 +0300

    Trying to get Heroku to work

--- server/app.js
+++ server/app.js
@@ -10,15 +10,14 @@ const app = express();
 
 app.set('port', process.env.PORT || 3001);
 
-const staticFiles = express.static(path.join(__dirname, '../../client/build'));
-app.use(staticFiles);
+if (process.env.NODE_ENV === 'production') {
+  const staticFiles = express.static(path.join(__dirname, '/../client/build'));
+  app.use(staticFiles);
+  // app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
+// MongodDb connection
 mongoose.Promise = global.Promise;
-
 if (process.env.NODE_ENV !== 'production') {
   mongoose.connect('mongodb://localhost/universocial');
 }
@@ -26,6 +25,14 @@ if (process.env.NODE_ENV !== 'production') {
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
 
 router(app);
 
