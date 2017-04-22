03cca04b1f1c0657b457f7a0de6b407ff49f6e89
Robert Istok <robertistok94@gmail.com>
2017-04-22 09:55:43 +0300

    heroku

--- server/app.js
+++ server/app.js
@@ -1,44 +1,76 @@
-const express = require('express');
+// const express = require('express');
+//
+// const app = express();
+//
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const morgan = require('morgan');
+// const path = require('path');
+// const router = require('./router');
+//
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   next();
+// });
+//
+// const staticFiles = express.static(path.join(__dirname, '../../client/build'));
+// app.use(staticFiles);
+//
+// // if (process.env.NODE_ENV === 'production') {
+// //
+// //   app.use('/*', staticFiles);
+// // }
+//
+// // MongodDb connection
+// mongoose.Promise = global.Promise;
+// if (process.env.NODE_ENV !== 'production') {
+//   mongoose.connect('mongodb://localhost/universocial');
+// }
+//
+// app.use(morgan('combined'));
+// app.use(bodyParser.json({ type: '*/*' }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+//
+// router(app);
+//
+// app.use('/*', staticFiles);
+//
+// app.set('port', 3001);
+//
+// module.exports = app;
 
+import bodyParser from 'body-parser';
+import express from 'express';
+import path from 'path';
 const app = express();
 
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const path = require('path');
-const router = require('./router');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  next();
-});
+const router = express.Router();
 
 const staticFiles = express.static(path.join(__dirname, '../../client/build'));
 app.use(staticFiles);
 
-// if (process.env.NODE_ENV === 'production') {
-//
-//   app.use('/*', staticFiles);
-// }
+router.get('/cities', (req, res) => {
+  const cities = [
+    { name: 'New York City', population: 8175133 },
+    { name: 'Los Angeles', population: 3792621 },
+    { name: 'Chicago', population: 2695598 }
+  ];
+  res.json(cities);
+});
 
-// MongodDb connection
-mongoose.Promise = global.Promise;
-if (process.env.NODE_ENV !== 'production') {
-  mongoose.connect('mongodb://localhost/universocial');
-}
+app.use(router);
 
-app.use(morgan('combined'));
-app.use(bodyParser.json({ type: '*/*' }));
-app.use(bodyParser.urlencoded({ extended: false }));
-
-router(app);
-
+// any routes not picked up by the server api will be handled by the react router
 app.use('/*', staticFiles);
 
-app.set('port', 3001);
-
-module.exports = app;
+app.set('port', process.env.PORT || 3001);
+app.listen(app.get('port'), () => {
+  console.log(`Listening on ${app.get('port')}`);
+});
