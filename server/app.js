0e8108428942d073644d27ae2f1bf1af1c2f4d40
Robert Istok <robertistok94@gmail.com>
2017-04-22 10:59:59 +0300

    heroku

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import path from 'path';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 
-import router from './router';
+// import router from './router';
 
 const app = express();
 
@@ -14,6 +14,57 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const staticFiles = express.static(path.join(__dirname, '../../client/build'));
 app.use(staticFiles);
+
+const passport = require('passport');
+
+const AuthenticationController = require('./controllers/authentication');
+const SchedulesController = require('./controllers/schedules');
+const GroupsController = require('./controllers/groups');
+const CoursesController = require('./controllers/courses');
+const TeachersController = require('./controllers/teachers');
+const MessagesController = require('./controllers/messages');
+const UsersController = require('./controllers/users');
+
+const passportService = require('./services/passport');
+
+const requireSignin = passport.authenticate('local', { session: false });
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+const router = express.Router();
+
+router.post('/api/auth/login', requireSignin, AuthenticationController.signIn);
+router.get(
+  '/api/auth/mefromtoken',
+  requireAuth,
+  AuthenticationController.meFromToken
+);
+
+router.post('/api/schedules/new', requireAuth, SchedulesController.addNew);
+router.get('/api/schedules/get/:id', requireAuth, SchedulesController.getOne);
+
+router.get('/api/groups/getall', requireAuth, GroupsController.getGroups);
+router.post('/api/groups/getbyid', requireAuth, GroupsController.getGroupById);
+
+router.get('/api/teachers/getall', TeachersController.getTeachers);
+
+router.post(
+  '/api/courses/teachersforcoursetype',
+  requireAuth,
+  CoursesController.getCourseTeachingTeacher
+);
+
+router.get(
+  '/api/messages/:username',
+  requireAuth,
+  MessagesController.getConversationsOfUser
+);
+
+router.get('/api/users/all', requireAuth, UsersController.getAll);
+router.get(
+  '/api/users/autocomplete/:term',
+  requireAuth,
+  UsersController.usersForAutocomplete
+);
 
 app.use(router);
 
@@ -32,10 +83,10 @@ app.use('/*', staticFiles);
 //
 
 // MongodDb connection
-// mongoose.Promise = global.Promise;
-// if (process.env.NODE_ENV !== 'production') {
-//   mongoose.connect('mongodb://localhost/universocial');
-// }
+mongoose.Promise = global.Promise;
+if (process.env.NODE_ENV !== 'production') {
+  mongoose.connect('mongodb://localhost/universocial');
+}
 
 app.set('port', process.env.PORT || 3001);
 
