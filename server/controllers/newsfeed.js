e407cce4ee80fba141b8dcab0d9c59a13eaa6cbf
Robert Istok <robertistok94@gmail.com>
2017-05-19 13:26:54 +0300

    newsfeed stub

---
+++ server/controllers/newsfeed.js
@@ -0,0 +1,12 @@
+import Newsfeed from '../models/newsfeed';
+import Course from '../models/course';
+import Student from '../models/student';
+import Teacher from '../models/teacher';
+
+function getFeed(req, res, next) {}
+
+function addItem(req, res, next) {}
+
+function updateItem(req, res, next) {}
+
+function deleteItem(req, res, next) {}
